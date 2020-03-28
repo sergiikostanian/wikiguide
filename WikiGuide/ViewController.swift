a83ea4274cfda348ceae637ff2b7b28e5627aa1d
Sergii Kostanian <sk@madappgang.com>
2020-03-28 08:25:04 +0200

    Initial Commit

---
+++ WikiGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiGuide
+//
+//  Created by Sergii Kostanian on 28.03.2020.
+//  Copyright © 2020 Serhii Kostanian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
