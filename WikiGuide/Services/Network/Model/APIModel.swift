1adcd1f7273742c802576b38b5402de9be4a203c
Sergii Kostanian <sk@madappgang.com>
2020-03-28 11:35:48 +0200

    Make network layer basic implementation

---
+++ WikiGuide/Services/Network/Model/APIModel.swift
@@ -0,0 +1,17 @@
+//
+//  APIModel.swift
+//  WikiGuide
+//
+//  Created by Serhii Kostanian on 28.03.2020.
+//  Copyright © 2020 Serhii Kostanian. All rights reserved.
+//
+
+import Foundation
+
+public enum APIModel {
+    
+    public struct Article: Codable {
+        // TODO: implement
+    }
+    
+}
