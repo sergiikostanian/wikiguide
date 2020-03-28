b845de19843cec11260bfa2726591200d975530a
Sergii Kostanian <sk@madappgang.com>
2020-03-28 09:57:06 +0200

    Implement dependency injection

---
+++ WikiGuide/Services/Observer Container/ObserverContainer.swift
@@ -0,0 +1,9 @@
+//
+//  ObserverContainer.swift
+//  WikiGuide
+//
+//  Created by Sergii Kostanian on 28.03.2020.
+//  Copyright © 2020 Serhii Kostanian. All rights reserved.
+//
+
+import Foundation
